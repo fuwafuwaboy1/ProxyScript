bfe0e4c88b9521e66e9319a624d0059d2db8c31a
fuwafuwaboy1 <fuwafuwaboy.f1@gmail.com>
2024-05-14 06:50:13 +0000

    update

--- proxy.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const { createProxy } = require('proxy');
-
-const server = createProxy(http.createServer());
-server.listen(2560, () => {
-  var port = server.address().port;
-  console.log('HTTP(s) proxy server listening on port %d', port);
-});
